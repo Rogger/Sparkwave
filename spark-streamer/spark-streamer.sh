72adf47cea2fc1e845faea644abbc9ac71e4351f
Michael Rogger <michael.rogger@sti2.at>
2013-03-12 15:43:54 +0100

    Removed deprecated streamer script

--- spark-streamer/spark-streamer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CP=.
-SEP=':'
-
-CP="$CP$SEP./lib/junixsocket-1.3.jar"
-CP="$CP$SEP/home/skomazec/.m2/repository/spark/spark-streamer/1.0-SNAPSHOT/spark-streamer-1.0-SNAPSHOT.jar"
-CP="$CP$SEP/home/skomazec/.m2/repository/spark/spark-core/1.0-SNAPSHOT/spark-core-1.0-SNAPSHOT.jar"
-
-exec java -cp "$CP" -Xmx1024M at.sti2.spark.streamer.SparkStreamer "$@"

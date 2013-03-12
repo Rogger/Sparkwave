f1e471fba72a2cc77c117bc68b37997bd9242d0d
Michael Rogger <michael.rogger@sti2.at>
2013-03-12 16:10:34 +0100

    Renamed spark-dist to spark-project as this maven project represents the root project; Assembly of the distribution package is performed in the project spark-distribution

--- spark-dist/spark-streamer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec java -Xmx1024M -jar ../spark-streamer/target/spark-streamer-1.0-SNAPSHOT-jar-with-dependencies.jar "$@"
--- spark-dist/spark.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "$@"
-exec java -Xmx1024M -jar ../spark-wave/target/spark-wave-1.0-SNAPSHOT.jar "$@"
